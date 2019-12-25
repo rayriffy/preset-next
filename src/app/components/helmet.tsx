3fe80a75840804dd0a616be881d243cc4fd5b3f5
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-25 21:35:36 +0700

    :tada: Intitial C

---
+++ src/app/components/helmet.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Helmet as ReactHelmet } from 'react-helmet'
+
+const Helmet: React.FC = props => {
+  return (
+    <ReactHelmet
+      htmlAttributes={{ lang: 'en' }}
+      defaultTitle={`preset-next`}
+      titleTemplate={`%s · preset-next`}
+      link={[
+        {
+          href: `/static/icon.png`,
+          rel: 'shortcut icon',
+        },
+        {
+          href: `/static/icon.png`,
+          rel: 'apple-touch-icon',
+        },
+      ]}
+    />
+  )
+}
+
+export default Helmet
